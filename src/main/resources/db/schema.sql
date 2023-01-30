e0d40d507ba695c6bb200edf2a5bffc3a91142db
IceFruit-BcDad <975075138@qq.com>
2023-01-30 23:20:59 +0800

    添加数据库表结构文件->schema.sql

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS course
+(
+    id                BIGINT AUTO_INCREMENT NOT NULL,
+    classification_id BIGINT                NOT NULL,
+    name              VARCHAR(255)          NULL,
+    create_user_id    BIGINT                NOT NULL,
+    create_time       datetime              NULL,
+    last_modify_time  datetime              NULL,
+    CONSTRAINT pk_course PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS classification
+(
+    id        BIGINT AUTO_INCREMENT NOT NULL,
+    name      VARCHAR(255)          NULL,
+    level     INT                   NOT NULL,
+    parent_id BIGINT                NULL,
+    CONSTRAINT pk_classification PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user
+(
+    id          BIGINT AUTO_INCREMENT NOT NULL,
+    name        VARCHAR(255)          NULL,
+    type_id     BIGINT                NOT NULL,
+    create_time datetime              NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_type
+(
+    id   BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255)          NULL,
+    CONSTRAINT pk_usertype PRIMARY KEY (id)
+);
